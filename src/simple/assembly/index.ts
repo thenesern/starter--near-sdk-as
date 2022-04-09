e181ce1b4666ed9472584c082581a65a65579ec1
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-04-09 15:45:15 +0300

    Implemented the Practice I

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,26 +1,50 @@
-import { storage, Context } from "near-sdk-as"
+import { storage, Context } from "near-sdk-as";
 
-// return the string 'hello world'
-export function helloWorld(): string {
-  return 'hello world'
+/*---------------------- Practice I --------------------*/
+
+// TASK I
+
+/* export function helloWorld(): string {
+  return 'hello enes'
+} */
+// near view $CONTRACT helloWorld
+// return the string 'hello enes'
+
+// TASK II
+
+/* export function helloWorld(name: string): string {
+  return 'hello ' + name
+} */
+
+// near view $CONTRACT helloWorld '{\"name\": \"enes\"}' --accountId eneseren.testnet
+// return the string 'hello enes'
+
+// TASK III
+
+export function helloWorld(names: Array<string>): string {
+  return names.map<string>((name) => "hello " + name).join(` `);
 }
+// near view $CONTRACT helloWorld '{\"names\":[\"huso\",\"deniz\"]}' --accountId eneseren.testnet
+// return the string 'hello hosu hello deniz'
+
+/*------------------------------------------------------*/
 
 // read the given key from account (contract) storage
 export function read(key: string): string {
   if (storage.hasKey(key)) {
-    return `✅ Key [ ${key} ] has value [ ${storage.getString(key)!} ]`
+    return `✅ Key [ ${key} ] has value [ ${storage.getString(key)!} ]`;
   } else {
-    return `🚫 Key [ ${key} ] not found in storage. ( ${storageReport()} )`
+    return `🚫 Key [ ${key} ] not found in storage. ( ${storageReport()} )`;
   }
 }
 
 // write the given value at the given key to account (contract) storage
 export function write(key: string, value: string): string {
-  storage.set(key, value)
-  return `✅ Data saved. ( ${storageReport()} )`
+  storage.set(key, value);
+  return `✅ Data saved. ( ${storageReport()} )`;
 }
 
 // private helper method used by read() and write() above
 function storageReport(): string {
-  return `storage [ ${Context.storageUsage} bytes ]`
+  return `storage [ ${Context.storageUsage} bytes ]`;
 }
